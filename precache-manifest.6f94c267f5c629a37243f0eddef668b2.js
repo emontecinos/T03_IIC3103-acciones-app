b737d27e7ec44e92d55e586bb78299a59c06d57b
Eduardo Montecinos <efmontecinos@uc.cl>
2020-05-18 19:32:36 -0400

    Updates

---
+++ precache-manifest.6f94c267f5c629a37243f0eddef668b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3b30d28981e6c0c33cab64b78aa57c",
+    "url": "/T03_IIC3103-acciones-app/index.html"
+  },
+  {
+    "revision": "6c67dcaa766fc5a33236",
+    "url": "/T03_IIC3103-acciones-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0b94c76a4c6a41a61053",
+    "url": "/T03_IIC3103-acciones-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c67dcaa766fc5a33236",
+    "url": "/T03_IIC3103-acciones-app/static/js/2.5c986e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/T03_IIC3103-acciones-app/static/js/2.5c986e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b94c76a4c6a41a61053",
+    "url": "/T03_IIC3103-acciones-app/static/js/main.46a82bfe.chunk.js"
+  },
+  {
+    "revision": "5d2ced2714990da7cb7f",
+    "url": "/T03_IIC3103-acciones-app/static/js/runtime-main.596e5d95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/T03_IIC3103-acciones-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
